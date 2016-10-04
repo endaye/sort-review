24b2d38b9187bda536d778acbfb5c38589480dbf
Yuancheng Zhang <old.far.hot@gmail.com>
2016-10-04 13:08:03 -0700

    added test

--- sort_method.cpp
+++ sort_method.cpp
@@ -6,6 +6,7 @@
 #include <stdio.h>
 
 void SortMethod::OutputResult(int * input_array, int array_size) {
+    printf("%s\n", sort_method_name());
     for (int i = 0; i < array_size; ++i) {
         printf("%2d ", input_array[i]);
     }
--- sort_method.h
+++ sort_method.h
@@ -8,6 +8,21 @@
 class SortMethod {
 public:
     void OutputResult(int *, int);
+    const char * sort_method_name() const {
+        return sort_method_name_;
+    }
+
+private:
+    /*
+     * 这里用下面这个会报错
+     * const char sort_method_name_[] = "General Sort";
+     * 必须要用
+     * const char sort_method_name_[13] = "General Sort";
+     * 或者（优先用此）
+     * const char *sort_method_name_ = "General Sort";
+     */
+    const char *sort_method_name_ = "General Sort";
+
 };
 
 
